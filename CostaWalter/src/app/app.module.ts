70750d1bf5787470eec098fa59f94e1b1446ae0b
WalterDarioCosta <107436201+WalterDarioCosta@users.noreply.github.com>
2022-07-30 21:18:24 -0300

    banner

--- CostaWalter/src/app/app.module.ts
+++ CostaWalter/src/app/app.module.ts
@@ -7,12 +7,20 @@ import { AppComponent } from './app.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { ComponentsComponent } from './components/components.component';
 import { HysComponent } from './components/hys/hys.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponentsComponent,
-    HysComponent
+    HysComponent,
+    HeaderComponent,
+    LogoApComponent,
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
